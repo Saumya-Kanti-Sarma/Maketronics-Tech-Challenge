647a1482d6915804ff146e5ca1a1f48647998d50
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-07-11 09:25:06 +0530

    updated routes

---
+++ backend/app/routes/routes.py
@@ -0,0 +1,80 @@
+from fastapi import APIRouter, HTTPException
+from bson import ObjectId
+from app.db.database import collection
+from app.models.models import CreateProductModel, UpdateProductModel
+from typing import Optional, List
+router = APIRouter()
+
+def product_serializer(product) -> dict:
+    return {
+        "id": str(product["_id"]),
+        "title": product["title"],
+        "link": product["link"],
+        "image": product["image"],
+        "price": product["price"],
+        "keyword": product["keyword"]
+    }
+
+@router.post("/", response_model=dict)
+def create_product(product: CreateProductModel):
+    result = collection.insert_one(product.model_dump())
+    return {"message":"Product Created", "data":result}
+
+@router.get("/", response_model=list)
+def get_all_products():
+    products = collection.find()
+    return [product_serializer(product) for product in products]
+
+
+"""Filter"""
+@router.get("/filter", response_model=List[dict])
+def get_filters(
+    keywords: Optional[str] = None,
+    f_price: Optional[str] = None,
+    l_price: Optional[str] = None,
+    low_to_high:Optional[str]=None,
+):
+    query = {}
+    # filter keywords
+    if keywords:
+        query["$or"] =[{"keyword": {"$regex": keywords, "$options": "i"}}]
+
+    # Price range 
+    if f_price is not None and l_price is not None:
+        query["price"] = {"$gte": f_price, "$lte": l_price}
+
+    # Price Limit
+    elif l_price is not None:
+        query["price"] = {"$lte": l_price}
+
+    if(low_to_high =="true"):
+        products = collection.find(query).sort("price", 1)
+        return [product_serializer(item) for item in products]
+
+    products = collection.find(query)
+    return [product_serializer(item) for item in products]
+
+@router.get("/{product_id}", response_model=dict)
+def get_product(product_id: str):
+    product = collection.find_one({"_id": ObjectId(product_id)})
+    if not product:
+        raise HTTPException(status_code=404, detail="product not found")
+    return product_serializer(product)
+
+@router.put("/{product_id}", response_model=dict)
+def update_product(product_id: str, product: UpdateProductModel):
+    update_data = {k: v for k, v in product.dict().items() if v is not None}
+    result = collection.update_one({"_id": ObjectId(product_id)}, {"$set": update_data})
+    if result.matched_count == 0:
+        raise HTTPException(status_code=404, detail="product not found")
+    updated = collection.find_one({"_id": ObjectId(product_id)})
+    return product_serializer(updated)
+
+@router.delete("/{product_id}", response_model=dict)
+def delete_product(product_id: str):
+    result = collection.delete_one({"_id": ObjectId(product_id)})
+    if result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="product not found")
+    return {"message": "product deleted successfully"}
+
+
